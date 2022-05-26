0b5c6f782c86ad52eb79f8e046c507b851158bfc
Peter Work Laptop <petervsjim@gmail.com>
2022-05-26 15:38:44 +0800

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
 
+  const [data, setData] = useState("")
+
+    var config = {
+      method: "get",
+      url: "http://localhost:3000/api",
+      headers: {},
+    };
+  
+    axios(config)
+      .then(function (response) {
+        setData(JSON.stringify(response.data));
+      }) 
+      .catch(function (error) {
+        console.log(error);
+      });
+  
+
   return (
-    <p>Hello World</p>
-  )
+  <>
+  <p>{data}</p>
+  <p>Hello World</p>
+  </>
+  );
 }
 
-export default App
+export default App;
